f83054155dea9e169e7babd7e937568d31157a29
MichaelMachu <54794967+MichaelMachu@users.noreply.github.com>
2022-10-02 16:14:27 +0200

    Transformed into Exercise2

--- AlgoritmyII/AlgoritmyII.cpp
+++ AlgoritmyII/AlgoritmyII.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include "Exercise1.h"
 #include "Components.h"
+#include "Exercise2.h"
 
 void MeasureWrapperExample1(void*);
 void MeasureWrapperExample2(void*);
@@ -14,38 +15,47 @@ void MeasureWrapperExample4(void*);
 
 int main()
 {
-    // Dynamic allocation use only if it is necessary!
-    //Exercise1 *exercise1 = new Exercise1(5);
-    //exercise1->Example1();
-    //delete[] exercise1;
+    const int size = 3;
 
-    Exercise1 exercise1(50000, false);
-    //exercise1.Example1()  // Volani metody
-    
-    //Components::PrintElements(exercise1.GetElements(), exercise1.GetElementsLength());    // Vypise vystup pole
-    
-    Components::MeasureMethod(&MeasureWrapperExample1, &exercise1);     // Provede mereni nad metodou
-    exercise1.ResetElements();                                          // Resetuje zpet pole do puvodniho stavu
-    Components::MeasureMethod(&MeasureWrapperExample2, &exercise1);
-    exercise1.ResetElements();
-    Components::MeasureMethod(&MeasureWrapperExample3, &exercise1);
-    exercise1.ResetElements();
-    Components::MeasureMethod(&MeasureWrapperExample4, &exercise1);
+    float matrixA1[size][size] = { {2, -1, 1}, {4, 1, -1}, {1, 1, 1} };
+    float elementsB1[size] = { 1, 5, 0 };
+
+    float matrixA2[size][size] = { {1, -1, 2}, {0, 0, -1}, {0, 2, -1} };
+    float elementsB2[size] = { 8, -11, -3 };
+
+    float matrixA3[size][size] = { {3, 2, -4}, {2, 3, 3}, {5, -3, 1} };
+    float elementsB3[size] = { 3, 15, 14 };
+
+    Exercise2 exercise2;
+    //exercise2.GaussianForwardElimination<size, size>(matrixA1, size, elementsB1, Exercise2::ForwardElimination);
+    //std::cout << "-------------------------------" << std::endl;
+    //exercise2.GaussianForwardElimination<size, size>(matrixA2, size, elementsB2, Exercise2::ForwardElimination);
+    //std::cout << "-------------------------------" << std::endl;
+    //exercise2.GaussianForwardElimination<size, size>(matrixA3, size, elementsB3, Exercise2::ForwardElimination);
+
+    //exercise2.GaussianForwardElimination<size, size>(matrixA1, size, elementsB1, Exercise2::BetterForwardElimination);
+    //std::cout << "-------------------------------" << std::endl;
+    //exercise2.GaussianForwardElimination<size, size>(matrixA2, size, elementsB2, Exercise2::BetterForwardElimination);
+    //std::cout << "-------------------------------" << std::endl;
+    //exercise2.GaussianForwardElimination<size, size>(matrixA3, size, elementsB3, Exercise2::BetterForwardElimination);
 
     // ----------- Domaci uloha -----------
     /*
-        Vytvorte novou tridu, ktera bude resit problem "Exercises 6.1 - Task 7".
-        Algoritmus nemusite merit, staci pouze splnit zadani a zavolat metodu, ktera resi problem a ukazuje spravny vystup.
+        Vytvorte novou tridu Task 2, ktera bude resit problem "Task 2".
+        
+        Pro reseni vyuzijte strukturu Equation, kterou budete zasilat jako parametr do funkce.
+        Pozor: neprepisujte hodnoty ve strukture Equation, metody budou vracet nove pole!
 
-        Nezapomente, ze algoritmus musi mit lepsi slozitost, nez kvadratickou, tzn. ze lepsi jak O(n^2)!!
-        Napoveda: vyuzijte trideni pred provedenim algoritmu stejne, jak je tomu u PresortElementUniqueness a PresortMode.
+        Priklad metod:
+        float **Task2::DecompositionLU(Equation equation);
+        float *Task2::DecompositionSolving(Equation equation, float **matrixLU);
+
     */
 
-    /* Exercises 6.1 - Task 7
-    You have an array of n real numbers and another integer s. Find out whether
-    the array contains two elements whose sum is s. (For example, for the array 5,
-    9, 1, 3 and s = 6, the answer is yes, but for the same array and s = 7, the answer is no.) 
-    Design an algorithm for this problem with a better than quadratic time efficiency.
+    /* Task 2
+    *   Vytvorte dve metody, ktere budou resit soustavu rovnic resenim LU rozkladem matice.
+    *   Prvni metoda bude obsahovat vypocet samotneho LU rozkladu, ktery pak nasledne bude vracet jako jednu matici.
+    *   Druha metoda pak ze samotneho LU rozkladu vypocita vysledky jednotlivych x-ovych hodnot.
     */
     
     
